d1c1303f6f1576e46887ead16b74365cafa1fbbc
Leandro Lucarella <luca-frequenz@llucax.com>
2026-08-17 08:59:24 +0000

    Allow overriding container user identity
    Use the CONTAI_* values when provided and retain host account values by
    default. Document the build-time overrides for custom container
    mappings.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -eu
 
 docker build \
 	-t contai:latest \
-	--build-arg UID=$(id -u) \
-	--build-arg USERNAME=$(id -un) \
-	--build-arg GID=$(id -g) \
-	--build-arg GROUPNAME=$(id -gn) \
+	--build-arg "UID=${CONTAI_UID:-$(id -u)}" \
+	--build-arg "USERNAME=${CONTAI_USER:-$(id -un)}" \
+	--build-arg "GID=${CONTAI_GID:-$(id -g)}" \
+	--build-arg "GROUPNAME=${CONTAI_GROUP:-$(id -gn)}" \
 	"$@" \
 	-f Dockerfile \
 	.
